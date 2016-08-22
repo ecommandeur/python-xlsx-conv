b0bd5dadd66a42a734ad8c50fad6fe0d6065334d
ecommand <ecommandeur@nl.dimins.com>
2016-08-23 01:27:41 +0200

    added version arg, added error handling, made messages more consistent

--- xlsx-conv.py
+++ xlsx-conv.py
@@ -1,28 +1,31 @@
 from openpyxl import load_workbook
-import csv
 import argparse
+import csv
 import os
 
 # ---
 # Get arguments
 # ---
+# TODO: let user specify delimiter
+# TODO: let user specify extension (csv, txt)
+# TODO: let user specify encoding
 
 parser = argparse.ArgumentParser(description='Convert XLSX file to CSV using openpyxl')
 parser.add_argument('-i','--input', help='Full path to XLSX file', required=True)
-parser.add_argument('-o','--outputDir', help='Full path output directory', required=False)
-parser.add_argument('--noprefix', help='Do not prefix ouput with workbook name', nargs='?', const=1, default=0, required=False)
-parser.add_argument('--prefix', help='Use specified prefix instead of prefixing output with workbook name', required=False)
+parser.add_argument('-o','--outputDir', help='Full path output directory')
+parser.add_argument('--noprefix', help='Do not prefix ouput with workbook name', nargs='?', const=1, default=0)
+parser.add_argument('--prefix', help='Use specified prefix instead of prefixing output with workbook name')
+parser.add_argument('--version', action='version', version='%(prog)s 1.0.0dev')
 args = parser.parse_args()
 
 inputPath = args.input
 outputDir = args.outputDir
 noPrefix = args.noprefix
 customPrefix = args.prefix
-print("noPrefix =", noPrefix)
 
 if not os.path.isfile(inputPath):
-    print("Cannot find ", inputPath)
-    parser.print_help()
+    print('xlsx-conv: error: No such file or directory:', inputPath)
+    parser.print_usage()
     exit(1)
 
 inputDir, inputFile = os.path.split(inputPath)
@@ -30,8 +33,8 @@ inputBaseFn, inputExt = os.path.splitext(inputFile)
 
 if outputDir:
     if not os.path.isdir(outputDir):
-        print("Cannot find ", outputDir)
-        parser.print_help()
+        parser.print_usage()
+        print('xlsx-conv: error: No such file or directory:', outputDir)
         exit(1)
 else:
    outputDir = inputDir
@@ -42,13 +45,24 @@ print("Dumping", inputPath)
 # Go ahead and dump that Workbook
 # ---
 
+# convert sheet function
+
 def convertSheet(ws,outputPath):
     with open(outputPath, 'w', encoding='utf-8') as f:
+        #c = csv.writer(f, lineterminator='\n', delimiter=',')
         c = csv.writer(f, lineterminator='\n')
         for row in ws.rows:
             c.writerow([cell.value for cell in row])
 
-wb = load_workbook(filename=inputPath, read_only=True, data_only=True)
+# load workbook and invoke convertSheet for all sheets in workbook
+
+try:
+    wb = load_workbook(filename=inputPath, read_only=True, data_only=True)
+except Exception as e:
+    print("xlsx-conv: error: Failed to load workbook")
+    print(e)
+    exit(1)
+    
 ws_names = wb.get_sheet_names()
 
 outPrefix = inputBaseFn + '.'
@@ -57,11 +71,15 @@ if customPrefix:
 if noPrefix == 1:
     outPrefix = '' # noprefix takes precedence over customPrefix
 
-
 for ws_name in ws_names:
     ws = wb[ws_name] # ws is now an IterableWorksheet
     outputPath = outputDir + os.sep + outPrefix + ws_name + '.csv'
     print("Outputting sheet to", outputPath)
-    convertSheet(ws,outputPath)
+    try:
+        convertSheet(ws,outputPath)
+    except Exception as e:
+        print("xlsx-conv: error: Failed to convert sheet")
+        print(e)
+        exit(1) 
 
-print("Outta here!")
+print("bye!")
