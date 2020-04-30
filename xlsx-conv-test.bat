4d3649038f64015246874b288ff24efc7166b372
Edwin Commandeur <e.commandeur@nl.dimins.com>
2020-05-01 00:42:56 +0200

    more tests

--- xlsx-conv-test.bat
+++ xlsx-conv-test.bat
@@ -13,6 +13,7 @@ REM # and subsequently calling this script
 REM #
 if exist temp\ rmdir /S/Q temp\
 mkdir temp
+
 REM #
 REM # Test simplest call only specifying xlsx/xltx input
 REM # -i is the only required argument
@@ -29,12 +30,14 @@ call python xlsx-conv.py -i "temp\Expenses.xltx"
 call FC temp\Expenses.Sheet1.csv resources\converted\Expenses.Sheet1.csv
 if %ERRORLEVEL%==1 goto failure
 if %ERRORLEVEL%==2 goto fcfailure
+
 REM #
 REM # Test different extension
 REM #
 call python xlsx-conv.py -i "temp\Characters.xlsx" --extension "test.csv"
 call dir /B "temp\Characters.Characters.test.csv"
 if %ERRORLEVEL%==1 goto failure
+
 REM #
 REM # Test different delimiter
 REM #
@@ -42,6 +45,7 @@ call python xlsx-conv.py -i "temp\Characters.xlsx" --delimiter ";" --extension "
 call FC temp\Characters.Characters.semicolon.csv resources\converted\Characters.Characters.semicolon.csv
 if %ERRORLEVEL%==1 goto failure
 if %ERRORLEVEL%==2 goto fcfailure
+
 REM #
 REM # Test line break replacement
 REM #
@@ -49,6 +53,7 @@ call python xlsx-conv.py -i "temp\Characters.xlsx" --linebreak_replacement " " -
 call FC temp\Characters.Characters.lr.csv resources\converted\Characters.Characters.lr.csv
 if %ERRORLEVEL%==1 goto failure
 if %ERRORLEVEL%==2 goto fcfailure
+
 REM #
 REM # Test noprefix
 REM # Output should be identical to automatically prefixed version
@@ -57,6 +62,7 @@ call python xlsx-conv.py -i "temp\Characters.xlsx" --noprefix
 call FC temp\Characters.csv resources\converted\Characters.Characters.csv
 if %ERRORLEVEL%==1 goto failure
 if %ERRORLEVEL%==2 goto fcfailure
+
 REM #
 REM # Test custom prefix
 REM # Output should be identical to automatically prefixed version
@@ -65,6 +71,7 @@ call python xlsx-conv.py -i "temp\Characters.xlsx" --prefix "CustomPrefix"
 call FC temp\CustomPrefix.Characters.csv resources\converted\Characters.Characters.csv
 if %ERRORLEVEL%==1 goto failure
 if %ERRORLEVEL%==2 goto fcfailure
+
 REM #
 REM # Test output dir
 REM #
@@ -72,6 +79,7 @@ call python xlsx-conv.py -i "resources\Formulas.xlsx" --o "temp"
 call FC temp\Formulas.Formulas.csv resources\converted\Formulas.Formulas.csv
 if %ERRORLEVEL%==1 goto failure
 if %ERRORLEVEL%==2 goto fcfailure
+
 REM #
 REM # Test row and column index
 REM #
@@ -79,6 +87,7 @@ call python xlsx-conv.py -i "resources\Formulas.xlsx" --o "temp" --col_index --r
 call FC temp\Formulas.Formulas.rc_index.csv resources\converted\Formulas.Formulas.rc_index.csv
 if %ERRORLEVEL%==1 goto failure
 if %ERRORLEVEL%==2 goto fcfailure
+
 REM #
 REM # Test max columns
 REM #
@@ -86,11 +95,44 @@ call python xlsx-conv.py -i "resources\Formulas.xlsx" --o "temp" --col_index --r
 call FC temp\Formulas.Formulas.max3.csv resources\converted\Formulas.Formulas.max3.csv
 if %ERRORLEVEL%==1 goto failure
 if %ERRORLEVEL%==2 goto fcfailure
+
 REM #
-REM # Test TXT input
+REM # Test encoding
+REM # ascii and latin-1 will fail on Formulas.xlsx because some characters cannot be encoded in ascii / latin-1
+REM # FC will give Resync Failed.  Files are too different. when comparing to UTF-8
+REM #
+call python xlsx-conv.py -i "resources\Formulas.xlsx" --o "temp" --encoding "utf-16" --extension "utf-16.csv"
+call FC temp\Formulas.Formulas.utf-16.csv resources\converted\Formulas.Formulas.utf-16.csv
+if %ERRORLEVEL%==1 goto failure
+if %ERRORLEVEL%==2 goto fcfailure
+
+REM #
+REM # Test if empty sheets are skipped
+REM # xlsx-conv should not fail on empty sheets!
+REM #
+call python xlsx-conv.py -i "resources\Empty.xlsx" --o "temp"
+if %ERRORLEVEL%==1 goto failure
+REM # we expect sheet to be empty so calling dir should set errorlevel to 1
+call dir /B "temp\Empty.Sheet1.csv"
+if %ERRORLEVEL%==0 goto failure
+
+REM #
 REM # clean test folder
 REM #
 call del /S /Q temp\*.*
+
+REM #
+REM # Test TXT input
+REM #
+call copy "resources\Characters.xlsx" "temp"
+call copy "resources\Numbers.xlsx" "temp"
+call python xlsx-conv.py -i "resources\Input.txt"
+call FC temp\Characters.Characters.csv resources\converted\Characters.Characters.csv
+if %ERRORLEVEL%==1 goto failure
+if %ERRORLEVEL%==2 goto fcfailure
+call FC temp\Numbers.Numbers.csv resources\converted\Numbers.Numbers.csv
+if %ERRORLEVEL%==1 goto failure
+if %ERRORLEVEL%==2 goto fcfailure
 call python xlsx-conv.py -i "resources\Input_OutputDir.txt"
 call FC temp\C.Characters.csv resources\converted\Characters.Characters.csv
 if %ERRORLEVEL%==1 goto failure
@@ -98,9 +140,27 @@ if %ERRORLEVEL%==2 goto fcfailure
 call FC temp\N.Numbers.csv resources\converted\Numbers.Numbers.csv
 if %ERRORLEVEL%==1 goto failure
 if %ERRORLEVEL%==2 goto fcfailure
-REM
-REM 
-REM
+
+REM #
+REM # Test TXT input
+REM #
+call python xlsx-conv.py -i "resources\Input_Sheet.txt" --noprefix --col_index --row_index --max_cols 4 --extension "max4.csv"
+call dir /B "temp\Sheet1.max4.csv"
+if %ERRORLEVEL%==0 goto failure
+call dir /B "temp\Sheet2.max4.csv"
+if %ERRORLEVEL%==1 goto failure
+call FC temp\Sheet2.max4.csv resources\converted\MultipleSheets.Sheet2.max4.csv
+if %ERRORLEVEL%==1 goto failure
+if %ERRORLEVEL%==2 goto fcfailure
+
+REM #
+REM # Test echo sheetnames
+REM #
+python xlsx-conv.py -i "resources\MultipleSheets.xlsx" --sheetnames > temp\MultipleSheets.sheet_names.txt
+call FC temp\MultipleSheets.sheet_names.txt resources\converted\MultipleSheets.sheet_names.txt
+if %ERRORLEVEL%==1 goto failure
+if %ERRORLEVEL%==2 goto fcfailure
+
 REM # Use ERRORLEVEL to check run
 echo %ERRORLEVEL%
 if %ERRORLEVEL%==0 goto success
